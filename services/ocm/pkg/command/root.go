680423707cf886f0ead0cdf72c5aed7d993e1dc5
Christian Richter <c.richter@opencloud.eu>
2025-12-02 15:27:48 +0100

    migrate ocm from urfave/cli to spf13/cobra
    Signed-off-by: Christian Richter <c.richter@opencloud.eu>

--- services/ocm/pkg/command/root.go
+++ services/ocm/pkg/command/root.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/opencloud-eu/opencloud/pkg/clihelper"
 	"github.com/opencloud-eu/opencloud/services/ocm/pkg/config"
-	"github.com/urfave/cli/v2"
+
+	"github.com/spf13/cobra"
 )
 
 // GetCommands provides all commands for this service
-func GetCommands(cfg *config.Config) cli.Commands {
-	return []*cli.Command{
+func GetCommands(cfg *config.Config) []*cobra.Command {
+	return []*cobra.Command{
 		// start this service
 		Server(cfg),
 
@@ -24,11 +25,12 @@ func GetCommands(cfg *config.Config) cli.Commands {
 
 // Execute is the entry point for the ocm command.
 func Execute(cfg *config.Config) error {
-	app := clihelper.DefaultApp(&cli.App{
-		Name:     "ocm",
-		Usage:    "starts ocm service",
-		Commands: GetCommands(cfg),
+	app := clihelper.DefaultAppCobra(&cobra.Command{
+		Use:   "ocm",
+		Short: "starts ocm service",
 	})
+	app.AddCommand(GetCommands(cfg)...)
+	app.SetArgs(os.Args[1:])
 
-	return app.RunContext(cfg.Context, os.Args)
+	return app.ExecuteContext(cfg.Context)
 }
