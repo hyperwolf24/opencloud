d89054484de50de97b53df48e087a291bc562c95
Christian Richter <c.richter@opencloud.eu>
2025-12-03 15:01:38 +0100

    migrate oc root cli command from urfave/cli to spf13/cobra
    Signed-off-by: Christian Richter <c.richter@opencloud.eu>

--- opencloud/pkg/command/root.go
+++ opencloud/pkg/command/root.go
@@ -9,25 +9,23 @@ import (
 	"github.com/opencloud-eu/opencloud/opencloud/pkg/register"
 	"github.com/opencloud-eu/opencloud/pkg/clihelper"
 	"github.com/opencloud-eu/opencloud/pkg/config"
-	"github.com/urfave/cli/v2"
+
+	"github.com/spf13/cobra"
 )
 
 // Execute is the entry point for the opencloud command.
 func Execute() error {
 	cfg := config.DefaultConfig()
 
-	app := clihelper.DefaultApp(&cli.App{
-		Name:  "opencloud",
-		Usage: "opencloud",
+	app := clihelper.DefaultAppCobra(&cobra.Command{
+		Use:   "opencloud",
+		Short: "opencloud",
 	})
 
 	for _, fn := range register.Commands {
-		app.Commands = append(
-			app.Commands,
-			fn(cfg),
-		)
+		app.AddCommand(fn(cfg))
 	}
-
+	app.SetArgs(os.Args[1:])
 	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
-	return app.RunContext(ctx, os.Args)
+	return app.ExecuteContext(ctx)
 }
